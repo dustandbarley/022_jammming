d30dc8b40acad0e62c5d99c32fdad6a916d8f177
dustandbarley <dustandbarley@gmail.com>
2024-03-15 16:48:41 +0000

    Step 45 Codecademy Jammming Project

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,21 +1,73 @@
-import React from "react";
-import styles from ".App.module.css";
+import React, { useState } from "react";
+import styles from "./App.module.css";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 import SearchBar from "../SearchBar/SearchBar";
 
+
 function App () {
+  
+  const [searchResults, setSearchResults] = useState([
+    {
+      name: "Example Track Name 1",
+      artist: "Example Track Artist 1",
+      album: "Example Track Album 1",
+      id: 1,
+    },
+    {
+      name: "Example Track Name 2",
+      artist: "Example Track Artist 2",
+      album: "Example Track Album 2",
+      id: 2,
+    },
+  ]);
+
+  const [playlistName, setPlaylistName] = useState("Example Playlist Name.");
+  const [playlistTracks, setPlaylistTracks] = useState ([
+    {
+      name: "Example Playlist Name 1",
+      artist: "Example Playlist Artist 1",
+      album: "Example Playlist Album 1",
+      id: 1,
+    },
+    {
+      name: "Example Playlist Name 2",
+      artist: "Example Playlist Artist 2",
+      album: "Example Playlist Album 2",
+      id: 2,
+    },
+    {
+      name: "Example Playlist Name 3",
+      artist: "Example Playlist Artist 3",
+      album: "Example Playlist Album 3",
+      id: 3,
+    },
+  ]);
+
+  function addTrack(track) {
+    const existingTrack = playlistTracks.find(t => t.id === track.id)
+    const newTrack = playlistTracks.concat(track);
+    if (existingTrack) {
+      console.log("Track already exists");
+    }
+  }
+
     return (
         <div>
         <h1>
-          Ja<span className="highlight">mmm</span>ing
+          Ja<span className={styles.highlight}>mmm</span>ing
         </h1>
-        <div className="App">
+        <div className={styles.App}>
           {/* <!-- Add a SearchBar component --> */}
           
-          <div className="App-playlist">
+          <div className={styles["App-playlist"]}>
             {/* <!-- Add a SearchResults component --> */}
+            <SearchResults userSearchResults={searchResults} />
             {/* <!-- Add a Playlist component --> */}
+            <Playlist 
+              playlistName={playlistName} 
+              playlistTracks={playlistTracks}
+            />
           </div>
         </div>
       </div>
@@ -23,3 +75,4 @@ function App () {
 }
 
 export default App;
+
---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./SearchResults.module.css";
+import Tracklist from "../Tracklist/Tracklist";
+
+
+function SearchResults (props) {
+    return (
+        <div className={styles.SearchResults}>
+        {/* <!-- Add a TrackList component --> */}
+        <Tracklist
+          userSearchResults={props.userSearchResults}
+          isRemoval={true} 
+          onAdd={props.onAdd}
+        />
+      </div>
+        );
+}
+
+export default SearchResults;
